c770aa07d8393a9b5440ac21e99495fd2fbf7a92
Maanav Shah <shah.maanav.07@gmail.com>
2025-08-03 23:56:17 +0530

    Update strategy_pattern.go

--- strategy_pattern.go
+++ strategy_pattern.go
@@ -1,54 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type PaymentInterface interface {
-	Pay()
+	Pay(amount float64) error
 }
 
-type CreditCard struct{}
-
-func (c *CreditCard) Pay() {
-	fmt.Println("Payment using CreditCard")
+type CreditCard struct {
+	CardNumber string
 }
 
-type UPI struct{}
-
-func (c *UPI) Pay() {
-	fmt.Println("Payment using UPI")
-}
-
-// Maintains a reference to a strategy object and allows dynamically changing the strategy
-type PaymentContext struct {
-	payment PaymentInterface
-}
-
-func NewPayment() PaymentContext {
-	return PaymentContext{}
-}
-
-func (p *PaymentContext) SetStrategy(paymentMode PaymentInterface) {
-	p.payment = paymentMode
-}
-
-func (p *PaymentContext) Pay() {
-	if p.payment == nil {
-		fmt.Println("No payment strategy set")
-		return
+func (c *CreditCard) Pay(amount float64) error {
+	if amount <= 0 {
+		return errors.New("invalid amount")
 	}
-	p.payment.Pay()
+	fmt.Printf("Paid $%.2f using Credit Card ****%s\n",
+		amount, c.CardNumber[len(c.CardNumber)-4:])
+	return nil
+}
+
+type UPI struct {
+	UPIId string
+}
+
+func (u *UPI) Pay(amount float64) error {
+	if amount <= 0 {
+		return errors.New("invalid amount")
+	}
+	fmt.Printf("Paid $%.2f using UPI ID: %s\n", amount, u.UPIId)
+	return nil
+}
+
+type PaymentContext struct {
+	strategy PaymentInterface
+}
+
+func NewPaymentContext() *PaymentContext {
+	return &PaymentContext{}
+}
+
+func (p *PaymentContext) SetStrategy(strategy PaymentInterface) error {
+	if strategy == nil {
+		return errors.New("strategy cannot be nil")
+	}
+	p.strategy = strategy
+	return nil
+}
+
+func (p *PaymentContext) Pay(amount float64) error {
+	if p.strategy == nil {
+		return errors.New("no payment strategy set")
+	}
+	return p.strategy.Pay(amount)
 }
 
 func main() {
-	payment := NewPayment()
-	payment.Pay()
+	payment := NewPaymentContext()
 
-	cc := &CreditCard{}
-	upi := &UPI{}
+	// Try payment without strategy
+	err := payment.Pay(100.0)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
 
+	// Credit Card payment
+	cc := &CreditCard{CardNumber: "1234567890123456"}
 	payment.SetStrategy(cc)
-	payment.Pay()
+	payment.Pay(150.50)
 
+	// UPI payment
+	upi := &UPI{UPIId: "user@paytm"}
 	payment.SetStrategy(upi)
-	payment.Pay()
+	payment.Pay(75.25)
 }
